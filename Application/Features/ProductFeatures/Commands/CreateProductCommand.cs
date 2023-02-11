3e5dd330e085aebb4d1f547b1ef51ddde66e11cf
Adithya Vijay K <adithyavj@gmail.com>
2023-02-11 14:29:58 +0530

    Added CQRS - Commands

---
+++ Application/Features/ProductFeatures/Commands/CreateProductCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductFeatures.Commands
+{
+    public class CreateProductCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal Rate { get; set; }
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public CreateProductCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+            {
+                var product = new Product
+                {
+                    Name = command.Name,
+                    Barcode = command.Barcode,
+                    Description = command.Description,
+                    Rate = command.Rate
+                };
+                await _context.Products.AddAsync(product);
+                await _context.SaveChanges();
+                return product.Id;
+            }
+        }
+    }
+}
---
+++ Application/Features/ProductFeatures/Commands/UpdateProductCommand.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProductFeatures.Commands
+{
+    public class UpdateProductCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+        public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public UpdateProductCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+            {
+                var product = await _context.Products.Where(p => p.Id == command.Id).FirstOrDefaultAsync();
+
+                if (product == null)
+                {
+                    return default;
+                }
+                else
+                {
+                    product.Name = command.Name;
+                    product.Barcode = command.Barcode;
+                    product.Description = command.Description;
+                    product.Rate = command.Rate;
+                    await _context.SaveChanges();
+                    return product.Id;
+                }
+            }
+        }
+    }
+}
