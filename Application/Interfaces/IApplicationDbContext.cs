9d22e66a167e5998bda6aef389c657a8bd562c69
Adithya Vijay K <adithyavj@gmail.com>
2023-02-11 13:05:05 +0530

    Added interfaces, DI extension class

---
+++ Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChanges();
+    }
+}
